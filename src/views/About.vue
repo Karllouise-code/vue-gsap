e4ea825941de372ce74b0ac092035ebe212e01f4
Karl Rito <karllouiserito08@gmail.com>
2024-12-11 11:58:06 +0800

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="about">
+    <transition
+      appear
+      name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+    >
+      <h1>About</h1>
+    </transition>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { gsap } from "gsap";
+export default {
+  setup() {
+    const beforeEnter = (el) => {
+      console.log("beforeEnter - set initial state", el);
+      el.style.transform = "translateY(-60px)";
+      el.style.opacity = 0;
+    };
+
+    const enter = (el, done) => {
+      console.log("starting to enter - make transition", el);
+
+      gsap.to(el, {
+        duration: 3,
+        y: 0,
+        opacity: 1,
+        ease: "bounce.out",
+        onComplete: done,
+      });
+    };
+
+    const afterEnter = (el) => {
+      console.log("afterEnter - set final state", el);
+    };
+
+    return {
+      beforeEnter,
+      enter,
+      afterEnter,
+    };
+  },
+};
+</script>
+
+<style>
+.about {
+  max-width: 600px;
+  margin: 20px auto;
+}
+</style>
